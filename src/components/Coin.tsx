62a55b35f7a9e0e6e93f65c7c292d18eee662dfe
pchchv <ipchchv@gmail.com>
2024-10-04 14:42:15 +0300

    Add some consts

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -1,3 +1,7 @@
+import { Platform, Pressable } from "react-native";
+import Animated from "react-native-reanimated";
+
+
 /**
 * @prop onClick - what happened on click the coin
 * @prop disabled - when coin can be clicked or not
@@ -5,4 +9,11 @@
 type CoinProps = {
     onClick: () => void;
     disabled?: boolean;
+};
+
+// Animated component to have ability use animated style from Reanimated package.
+const AnimatedButton = Animated.createAnimatedComponent(Pressable);
+const sensitivity = Platform.OS == "web" ? 0.1 : 0.2;
+const animationConfig = {
+    duration: 100,
 };
