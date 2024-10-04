89749f9dbe4b17fa023b99f5b0700155bb29bc98
pchchv <ipchchv@gmail.com>
2024-10-04 14:46:58 +0300

    Refactoring

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -2,6 +2,13 @@ import { Platform, Pressable } from "react-native";
 import Animated from "react-native-reanimated";
 
 
+// Animated component to have ability use animated style from Reanimated package.
+const AnimatedButton = Animated.createAnimatedComponent(Pressable);
+const sensitivity = Platform.OS == "web" ? 0.1 : 0.2;
+const animationConfig = {
+    duration: 100,
+};
+
 /**
 * @prop onClick - what happened on click the coin
 * @prop disabled - when coin can be clicked or not
@@ -9,11 +16,4 @@ import Animated from "react-native-reanimated";
 type CoinProps = {
     onClick: () => void;
     disabled?: boolean;
-};
-
-// Animated component to have ability use animated style from Reanimated package.
-const AnimatedButton = Animated.createAnimatedComponent(Pressable);
-const sensitivity = Platform.OS == "web" ? 0.1 : 0.2;
-const animationConfig = {
-    duration: 100,
 };
