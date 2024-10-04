e6dc887f2f85222dc694712ecabbfc99ba9cc0ce
pchchv <ipchchv@gmail.com>
2024-10-04 14:56:44 +0300

    Implement an useHaptics

--- src/components/useHaptics.ts
+++ src/components/useHaptics.ts
@@ -1,10 +1,34 @@
-import {
- NotificationFeedbackType,
- ImpactFeedbackStyle,
-} from "expo-haptics";
+import { Platform } from "react-native";
+import { useEffect, useState } from "react";
+import { impactAsync, notificationAsync, NotificationFeedbackType, ImpactFeedbackStyle } from "expo-haptics";
 
 
 type Haptics = {
     impactOccurred: (style: ImpactFeedbackStyle) => Promise;
     notificationOccurred: (type: NotificationFeedbackType) => Promise;
+};
+
+
+export const useHaptics = () => {
+    const [haptics, setHaptics] = useState({
+        impactOccurred: async _ => {},
+        notificationOccurred: async _ => {},
+    });
+    
+    useEffect(() => {
+        if (Platform.OS == "web") {
+            if (window.Telegram?.WebApp.HapticFeedback) {
+                setHaptics(window.Telegram.WebApp.HapticFeedback);
+                return;
+            }
+        }
+        
+        const impact = async (style: ImpactFeedbackStyle) =>
+            await impactAsync(style);
+        const notification = async (type: NotificationFeedbackType) =>
+            await notificationAsync(type);
+        setHaptics({ impactOccurred: impact, notificationOccurred: notification });
+    }, []);
+    
+    return haptics;
 };
