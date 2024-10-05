942a5c7062706cf3cd44ed3a3987f42ecac33142
pchchv <ipchchv@gmail.com>
2024-10-05 18:05:32 +0300

    Implement the Coin

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -1,5 +1,9 @@
-import { Platform, Pressable, StyleSheet } from "react-native";
-import Animated from "react-native-reanimated";
+import React, { useState } from "react";
+import { ImpactFeedbackStyle } from "expo-haptics";
+import Animated, {useSharedValue, withTiming, useAnimatedStyle, SlideOutUp } from "react-native-reanimated";
+import { Dimensions, Platform, Pressable, StyleSheet, GestureResponderEvent, View, Image, Text } from "react-native";
+import { useHaptics } from "@/useHaptics";
+import { generateUuid } from "@/utils";
 import { config } from "@/config";
 
 
@@ -7,7 +11,7 @@ import { config } from "@/config";
 const AnimatedButton = Animated.createAnimatedComponent(Pressable);
 const sensitivity = Platform.OS == "web" ? 0.1 : 0.2;
 const animationConfig = {
-    duration: 100,
+  duration: 100,
 };
 
 /**
@@ -15,8 +19,103 @@ const animationConfig = {
 * @prop disabled - when coin can be clicked or not
 */
 type CoinProps = {
-    onClick: () => void;
-    disabled?: boolean;
+  onClick: () => void;
+  disabled?: boolean;
+};
+
+export const Coin: React.FC<CoinProps> = ({ disabled, onClick }) => {
+  const [number, setNumber] = useState<
+  { id: string; x: number; y: number } | undefined
+  >(undefined);
+  const [showNumber, setShowNumber] = useState(false);
+  const width = Dimensions.get("window").width - 50;
+  // setting coin size based on window and check web compatibility
+  const size = width > 1000 ? 1000 : width;
+  const center = size / 2;
+  // shared values to use in coin animation
+  const rotateX = useSharedValue(0);
+  const rotateY = useSharedValue(0);
+  const { impactOccurred } = useHaptics();
+  const handlePressIn = async (e: GestureResponderEvent) => {
+    await impactOccurred(ImpactFeedbackStyle.Light);
+    const { locationX, locationY } = e.nativeEvent;
+    // getting rotate amount by x axis
+    const deltaX = locationX - center;
+    // getting rotate amount by y axis
+    const deltaY = locationY - center;
+    if (Platform.OS === "web") {
+      rotateY.value = deltaX * sensitivity;
+      rotateX.value = -deltaY * sensitivity;
+    } else {
+      rotateY.value = withTiming(deltaX * sensitivity, animationConfig);
+      rotateX.value = withTiming(-deltaY * sensitivity, animationConfig);
+    }
+    // set number position && unique id to have no problems with keys
+    setNumber({ id: generateUuid(), x: locationX, y: locationY });
+  };
+  
+  const handlePressOut = (e: GestureResponderEvent) => {
+    setShowNumber(true);
+    if (Platform.OS === "web") {
+      rotateX.value = 0;
+      rotateY.value = 0;
+    } else {
+      rotateX.value = withTiming(0, animationConfig);
+      rotateY.value = withTiming(0, animationConfig);
+    }
+    
+    onClick();
+
+    // use timeout to not remove element on render start
+    setTimeout(() => {
+      // set values undefined to launch exiting animation
+      setNumber(undefined);
+      setShowNumber(false);
+    }, 10);
+  };
+  
+  // style to define coin rotation
+  const rotateStyle = useAnimatedStyle(
+    () => ({
+      position: "relative",
+      transform: [
+        {
+          rotateY: `${rotateY.value}deg`,
+        },
+        {
+          rotateX: `${rotateX.value}deg`,
+        },
+      ],
+    }),
+    [rotateX, rotateY],
+  );
+  
+  return (
+    <View style={styles.container}>
+      <AnimatedButton
+      style={[rotateStyle]}
+      disabled={disabled}
+      onPressIn={handlePressIn}
+      onPressOut={handlePressOut}>
+        <Image
+        source={require("../../assets/icons/coin.png")}
+        style={{ height: size, width: size }}></Image>
+      </AnimatedButton>
+      {!!number && showNumber && (
+        <Animated.View
+        exiting={SlideOutUp.duration(500)}
+        key={number.id}
+        style={{
+          position: "absolute",
+          top: number.y,
+          left: number.x,
+          zIndex: 1000,
+        }}>
+          <Text style={[styles.text]}>+1</Text>
+        </Animated.View>
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
