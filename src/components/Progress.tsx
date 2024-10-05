4342a6225f7cd583440ec0fde4ba7450999832a7
pchchv <ipchchv@gmail.com>
2024-10-05 18:18:36 +0300

    Implement a styles

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,4 +1,39 @@
+import {StyleSheet} from 'react-native';
+import { config } from '@/config';
+
 type ProgressProps = {
   max?: number;
   amount: number;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: 70,
+    borderColor: config().themeParams.accent_text_color,
+    backgroundColor: config().themeParams.section_bg_color,
+    borderWidth: 2,
+    borderRadius: 70,
+    overflow: "hidden",
+    position: "relative",
+    justifyContent: "center",
+  },
+  progress: {
+    height: "100%",
+    backgroundColor: config().themeParams.accent_text_color,
+    width: 200,
+    borderRadius: 20,
+    position: "absolute",
+    justifyContent: "center",
+    overflow: "hidden",
+  },
+  text: {
+    fontWeight: "700",
+    fontSize: 24,
+    color: config().themeParams.accent_text_color,
+    textAlign: "center",
+  },
+  progressText: {
+    textAlign: "center",
+    color: config().themeParams.text_color,
+  },
+});
