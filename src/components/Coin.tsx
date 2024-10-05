22cde1d8e6c48dfde147104ec5e3706e91302da4
pchchv <ipchchv@gmail.com>
2024-10-05 18:08:25 +0300

    Use prettier

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -1,49 +1,62 @@
-import React, { useState } from "react";
-import { ImpactFeedbackStyle } from "expo-haptics";
-import Animated, {useSharedValue, withTiming, useAnimatedStyle, SlideOutUp } from "react-native-reanimated";
-import { Dimensions, Platform, Pressable, StyleSheet, GestureResponderEvent, View, Image, Text } from "react-native";
-import { useHaptics } from "@/useHaptics";
-import { generateUuid } from "@/utils";
-import { config } from "@/config";
-
+import React, {useState} from 'react';
+import {ImpactFeedbackStyle} from 'expo-haptics';
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  SlideOutUp,
+} from 'react-native-reanimated';
+import {
+  Dimensions,
+  Platform,
+  Pressable,
+  StyleSheet,
+  GestureResponderEvent,
+  View,
+  Image,
+  Text,
+} from 'react-native';
+import {useHaptics} from '@/useHaptics';
+import {generateUuid} from '@/utils';
+import {config} from '@/config';
 
 // Animated component to have ability use animated style from Reanimated package.
 const AnimatedButton = Animated.createAnimatedComponent(Pressable);
-const sensitivity = Platform.OS == "web" ? 0.1 : 0.2;
+const sensitivity = Platform.OS == 'web' ? 0.1 : 0.2;
 const animationConfig = {
   duration: 100,
 };
 
 /**
-* @prop onClick - what happened on click the coin
-* @prop disabled - when coin can be clicked or not
-*/
+ * @prop onClick - what happened on click the coin
+ * @prop disabled - when coin can be clicked or not
+ */
 type CoinProps = {
   onClick: () => void;
   disabled?: boolean;
 };
 
-export const Coin: React.FC<CoinProps> = ({ disabled, onClick }) => {
+export const Coin: React.FC<CoinProps> = ({disabled, onClick}) => {
   const [number, setNumber] = useState<
-  { id: string; x: number; y: number } | undefined
+    {id: string; x: number; y: number} | undefined
   >(undefined);
   const [showNumber, setShowNumber] = useState(false);
-  const width = Dimensions.get("window").width - 50;
+  const width = Dimensions.get('window').width - 50;
   // setting coin size based on window and check web compatibility
   const size = width > 1000 ? 1000 : width;
   const center = size / 2;
   // shared values to use in coin animation
   const rotateX = useSharedValue(0);
   const rotateY = useSharedValue(0);
-  const { impactOccurred } = useHaptics();
+  const {impactOccurred} = useHaptics();
   const handlePressIn = async (e: GestureResponderEvent) => {
     await impactOccurred(ImpactFeedbackStyle.Light);
-    const { locationX, locationY } = e.nativeEvent;
+    const {locationX, locationY} = e.nativeEvent;
     // getting rotate amount by x axis
     const deltaX = locationX - center;
     // getting rotate amount by y axis
     const deltaY = locationY - center;
-    if (Platform.OS === "web") {
+    if (Platform.OS === 'web') {
       rotateY.value = deltaX * sensitivity;
       rotateX.value = -deltaY * sensitivity;
     } else {
@@ -51,19 +64,19 @@ export const Coin: React.FC<CoinProps> = ({ disabled, onClick }) => {
       rotateX.value = withTiming(-deltaY * sensitivity, animationConfig);
     }
     // set number position && unique id to have no problems with keys
-    setNumber({ id: generateUuid(), x: locationX, y: locationY });
+    setNumber({id: generateUuid(), x: locationX, y: locationY});
   };
-  
+
   const handlePressOut = (e: GestureResponderEvent) => {
     setShowNumber(true);
-    if (Platform.OS === "web") {
+    if (Platform.OS === 'web') {
       rotateX.value = 0;
       rotateY.value = 0;
     } else {
       rotateX.value = withTiming(0, animationConfig);
       rotateY.value = withTiming(0, animationConfig);
     }
-    
+
     onClick();
 
     // use timeout to not remove element on render start
@@ -73,11 +86,11 @@ export const Coin: React.FC<CoinProps> = ({ disabled, onClick }) => {
       setShowNumber(false);
     }, 10);
   };
-  
+
   // style to define coin rotation
   const rotateStyle = useAnimatedStyle(
     () => ({
-      position: "relative",
+      position: 'relative',
       transform: [
         {
           rotateY: `${rotateY.value}deg`,
@@ -89,28 +102,28 @@ export const Coin: React.FC<CoinProps> = ({ disabled, onClick }) => {
     }),
     [rotateX, rotateY],
   );
-  
+
   return (
     <View style={styles.container}>
       <AnimatedButton
-      style={[rotateStyle]}
-      disabled={disabled}
-      onPressIn={handlePressIn}
-      onPressOut={handlePressOut}>
+        style={[rotateStyle]}
+        disabled={disabled}
+        onPressIn={handlePressIn}
+        onPressOut={handlePressOut}>
         <Image
-        source={require("../../assets/icons/coin.png")}
-        style={{ height: size, width: size }}></Image>
+          source={require('../../assets/icons/coin.png')}
+          style={{height: size, width: size}}></Image>
       </AnimatedButton>
       {!!number && showNumber && (
         <Animated.View
-        exiting={SlideOutUp.duration(500)}
-        key={number.id}
-        style={{
-          position: "absolute",
-          top: number.y,
-          left: number.x,
-          zIndex: 1000,
-        }}>
+          exiting={SlideOutUp.duration(500)}
+          key={number.id}
+          style={{
+            position: 'absolute',
+            top: number.y,
+            left: number.x,
+            zIndex: 1000,
+          }}>
           <Text style={[styles.text]}>+1</Text>
         </Animated.View>
       )}
@@ -119,13 +132,13 @@ export const Coin: React.FC<CoinProps> = ({ disabled, onClick }) => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-        position: "relative",
-    },
-    text: {
-        fontSize: 26,
-        fontWeight: "600",
-        // getting text color from Telegram client
-        color: config().themeParams?.hint_color,
-    },
+  container: {
+    position: 'relative',
+  },
+  text: {
+    fontSize: 26,
+    fontWeight: '600',
+    // getting text color from Telegram client
+    color: config().themeParams?.hint_color,
+  },
 });
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,84 +1,84 @@
-import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { config } from "@/config";
+import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {config} from '@/config';
 
 type HeaderProps = {
-    amount: number;
+  amount: number;
 };
 
-export const Header: React.FC<HeaderProps> = ({ amount }) => {
-    const insets = useSafeAreaInsets();
-    const paddingTop = Math.max(20, insets.top);
-    const { username, photo_url } = config().initDataUnsafe.user;
-    return (
-        <View style={[styles.header, { paddingTop }]}>
-            <View style={styles.amountRow}>
-                <Image
-                source={require("../../assets/icons/coin.png")}
-                style={{ height: 40, width: 40 }}
-                />            
-                <Text style={styles.text}>{amount}</Text>
-            </View>
-            <View style={styles.userInfo}>
-                <Text style={styles.username}>@{username}</Text>
-                {photo_url ? (
-                    <Image
-                    style={[styles.image, { backgroundColor: "transparent" }]}
-                    source={{
-                        uri: photo_url,
-                    }}></Image>
-                ) : (
-                <View style={styles.image}>
-                    <Image
-                    style={styles.icon}
-                    source={require("../../assets/icons/profile-placeholder.png")}
-                    />
-                </View>
-                )}
-            </View>
-       </View>
-    );
+export const Header: React.FC<HeaderProps> = ({amount}) => {
+  const insets = useSafeAreaInsets();
+  const paddingTop = Math.max(20, insets.top);
+  const {username, photo_url} = config().initDataUnsafe.user;
+  return (
+    <View style={[styles.header, {paddingTop}]}>
+      <View style={styles.amountRow}>
+        <Image
+          source={require('../../assets/icons/coin.png')}
+          style={{height: 40, width: 40}}
+        />
+        <Text style={styles.text}>{amount}</Text>
+      </View>
+      <View style={styles.userInfo}>
+        <Text style={styles.username}>@{username}</Text>
+        {photo_url ? (
+          <Image
+            style={[styles.image, {backgroundColor: 'transparent'}]}
+            source={{
+              uri: photo_url,
+            }}></Image>
+        ) : (
+          <View style={styles.image}>
+            <Image
+              style={styles.icon}
+              source={require('../../assets/icons/profile-placeholder.png')}
+            />
+          </View>
+        )}
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    header: {
-        backgroundColor: config().themeParams?.header_bg_color,
-        paddingHorizontal: 20,
-        flexDirection: "row",
-        alignItems: "center",
-        paddingBottom: 20,
-        justifyContent: "space-between",
-    },
-    amountRow: {
-        flexDirection: "row",
-        alignItems: "center",
-    },
-    text: {
-        fontSize: 24,
-        fontWeight: "600",
-        color: config().themeParams?.text_color,
-    },
-    userInfo: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 20,
-    },
-    username: {
-        color: config().themeParams.accent_text_color,
-        fontSize: 18,
-    },
-    image: {
-        backgroundColor: config().themeParams.button_color,
-        height: 50,
-        width: 50,
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: 50,
-    },
-    icon: {
-        height: 30,
-        width: 30,
-        tintColor: config().themeParams.text_color,
-    },
+  header: {
+    backgroundColor: config().themeParams?.header_bg_color,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingBottom: 20,
+    justifyContent: 'space-between',
+  },
+  amountRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: config().themeParams?.text_color,
+  },
+  userInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 20,
+  },
+  username: {
+    color: config().themeParams.accent_text_color,
+    fontSize: 18,
+  },
+  image: {
+    backgroundColor: config().themeParams.button_color,
+    height: 50,
+    width: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+  },
+  icon: {
+    height: 30,
+    width: 30,
+    tintColor: config().themeParams.text_color,
+  },
 });
