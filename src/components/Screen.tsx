8d7c2d1946c50b808207afd60ddbda36951db758
pchchv <ipchchv@gmail.com>
2024-10-06 06:13:31 +0300

    Implement a Screen

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,5 +1,10 @@
-import {StyleSheet} from 'react-native';
+import React from 'react';
+import { StyleSheet, View, ViewProps } from 'react-native';
 import {config} from '@/config';
+
+export const Screen: React.FC<ViewProps> = ({ children, style }) => {
+  return <View style={[styles.screen, style]}>{children}</View>;
+};
 
 const styles = StyleSheet.create({
   screen: {
